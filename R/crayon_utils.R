b033243a97cffe4f146074531e1ecdda80666b87
Miles McBain <miles.mcbain@gmail.com>
2021-07-30 16:36:18 +1000

    fix safely gaff

--- R/crayon_utils.R
+++ R/crayon_utils.R
@@ -1,8 +1,8 @@
 lapply_safely <- function(lst, funs) {
 	lapply(lst, function(x) {
 		if (is.null(x)) return(x)
-		if ((length(x) == 1)) {
-      if (!is.na(x) || !is.infinite(x)) return(x)
+		if (length(x) == 1) {
+      if (is.na(x) || is.infinite(x)) return(x)
 		}
 		funs(x)
 	})
