b026ab44cbdd8420f40dd182abb73d062e9638ef
Miles McBain <miles.mcbain@gmail.com>
2026-02-08 16:05:11 +1000

    add additional paging controls for ipaint
    fixes #9

--- R/ipaint.R
+++ R/ipaint.R
@@ -1,9 +1,20 @@
 #' Interatively scroll through rows of a painted dataframe
 #'
 #' This function calls [paint()] repeatedly on a dataframe,
-#' updating the rows shown in response to left or right keypresses.
+#' updating the rows shown in response to keypresses.
 #'
 #' It will only work in terminals supported by {keypress} - Not many!
+#'
+#' Controls:
+#'
+#' - 'l' or 'right arrow' forward 1 row
+#' - 'h' or 'left arrow' backward 1 row
+#' - 'enter' or 'escape' quit interactive painting
+#' - 'b' backward 1 page of rows
+#' - 'f' forward 1 page of rows
+#' - 'G' jump to final page of rows
+#' - 'g' jump to first page of rows
+#'
 #'
 #' @param df the dataframe to scroll through, defaults to `.Last.value`
 #' @export
@@ -14,7 +25,7 @@ ipaint <- function(df = .Last.value) {
   row <- 1
   palette <- getOption("paint_palette", rainbow_6())
   key <- ""
-  while (key != "enter") {
+  while (key != "enter" && key != "escape") {
     if (key == "left" || key == "h") {
       old_row <- row
       row <- max(1, row - 1)
@@ -23,16 +34,46 @@ ipaint <- function(df = .Last.value) {
       old_row <- row
       row <- min(nrow(df), row + 1)
       if (row > old_row) palette <- rotate_palette_forward(palette)
+    } else if (key == "f") {
+      # forward 1 page
+      next_page_row_start <-
+        row + getOption("paint_n_rows", length(palette))
+      if (next_page_row_start <= nrow(df)) {
+        row <- next_page_row_start
+        # palette stays the same as advancing exactly 1 page
+      }
+    } else if (key == "b") {
+      # back one page
+      previous_page_row_start <-
+        row - getOption("paint_n_rows", length(palette))
+      if (previous_page_row_start >= 1) {
+        row <- previous_page_row_start
+        # palette stays the same as retreating by exactly 1 page
+      }
+    } else if (key == "g") {
+      # first page
+      start_diff <- row - 1
+      row <- 1
+      if (start_diff > 0) palette <- rotate_palette_backward_n(palette, start_diff)
+    } else if (key == "G") {
+      # last page
+      last_page_row_start <-
+        nrow(df) - getOption("paint_n_rows", length(palette)) + 1
+      end_diff <- last_page_row_start - row
+      if (end_diff > 0) {
+        row <- last_page_row_start
+        palette <- rotate_palette_forward_n(palette, end_diff)
+      }
     }
     paint_to_output_buffer(df, palette = palette, start_row = row)
     row_footer_to_output_buffer(row, df, palette)
 
     if (key != "") {
-			buffer_size <- get_output_buffer_rows()
-			cat(sprintf("\033[%dA\r", buffer_size - 1)) # back to first row 
-			cat(blank_buffer(buffer_size))
-			cat(sprintf("\033[%dA\r", buffer_size - 1)) 
-		}
+      buffer_size <- get_output_buffer_rows()
+      cat(sprintf("\033[%dA\r", buffer_size - 1)) # back to first row
+      cat(blank_buffer(buffer_size))
+      cat(sprintf("\033[%dA\r", buffer_size - 1))
+    }
 
     cat(get_output_buffer(), sep = "")
     reset_output_buffer()
@@ -47,6 +88,22 @@ rotate_palette_forward <- function(palette) {
 
 rotate_palette_backward <- function(palette) {
   c(palette[[length(palette)]], palette[1:(length(palette) - 1)])
+}
+
+rotate_palette_forward_n <- function(palette, n) {
+  idx <- seq_along(palette)
+  idx_rotated <- (idx + n) %% length(palette)
+  idx_rotated[idx_rotated == 0] <- length(palette)
+  idx_rotated
+  palette[idx_rotated]
+}
+
+rotate_palette_backward_n <- function(palette, n) {
+  idx <- seq_along(palette)
+  idx_rotated <- (idx - n) %% length(palette)
+  idx_rotated[idx_rotated == 0] <- length(palette)
+  idx_rotated
+  palette[idx_rotated]
 }
 
 row_footer_to_output_buffer <- function(row, df, palette) {
@@ -74,6 +131,6 @@ row_footer_to_output_buffer <- function(row, df, palette) {
 }
 
 blank_buffer <- function(n_rows) {
-	line <- strrep(" ", getOption("paint_max_width", 60))
-	paste0(rep(line, n_rows), collapse = "\n")
+  line <- strrep(" ", getOption("paint_max_width", 60))
+  paste0(rep(line, n_rows), collapse = "\n")
 }
